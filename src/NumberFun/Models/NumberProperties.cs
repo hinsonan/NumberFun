e7b47eb7ed538587d7a225d797dd18461f6d2ada
hinsonan <hinsonan@gmail.com>
2017-03-11 22:32:45 -0600

    version 1.01 all methods implemented

--- src/NumberFun/Models/NumberProperties.cs
+++ src/NumberFun/Models/NumberProperties.cs
@@ -35,6 +35,30 @@ namespace NumberFun.Models
             
         }
 
+        //this method will be used in the isPowerful method
+        private bool isPrime(int num)
+        {
+            int modCount = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    modCount++;
+                }
+            }
+            if (modCount == 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+
         public bool isPerfect()
         {
             int sum = 0;
@@ -55,19 +79,77 @@ namespace NumberFun.Models
             }
         }
 
+
+
         public bool isSquareful()
         {
-            return true;
+            for (int i = 1; i <= Number; i++)
+            {
+                //if the number is evenly divisible by i do this
+                if (Number % i == 0)
+                {
+                    //if i is a prime number check if i^2 is divides evenly into Number
+                    if (isPrime(i) == true)
+                    {
+                        //if the i^2 does not divide evenly then the number is squareful
+                        if (Number % (i * i) == 0)
+                        {
+                            return true;
+                        }
+
+                    }
+                }
+            }
+            return false;
         }
 
         public bool isPowerful()
         {
+            for(int i = 1; i <= Number; i++)
+            {
+                //if the number is evenly divisible by i do this
+                if(Number % i == 0)
+                {
+                    //if i is a prime number check if i^2 is divides evenly into Number
+                    if(isPrime(i) == true)
+                    {
+                        //if the i^2 does not divide evenly then the number is not powerful
+                        if(Number % (i*i) != 0)
+                        {
+                            return false;
+                        }
+                        
+                    }
+                }
+            }
             return true;
         }
 
         public bool isPalindrome()
         {
-            return true;
+            int num = Number;
+            int remainder;
+            int reverse = 0;
+            int temp;
+
+            temp = num;
+            while (num > 0)
+            {
+                remainder = num % 10;
+                reverse = reverse * 10 + remainder;
+                num /= 10;
+            }
+
+            if (temp == reverse)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+                
+            
         }
     }
 }
